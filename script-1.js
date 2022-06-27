4abdd87a726184f9f8a91b973b7ce5ed90f117ce
Alona-Liubchenko <aylin888@rambler.ru>
2022-06-27 23:59:55 +0300

    h1

---
+++ script-1.js
@@ -0,0 +1,33 @@
+// const totalPrice = 300;
+// const massage = "Welcom!";
+// console.log(totalPrice);
+// console.log(massage);
+
+// const type = typeof 5;
+// console.log(type);
+
+// alert("qwer");
+// console.log(alert);
+
+// const should = confirm("нажми");
+// console.log(should);
+
+// let price = prompt("Введіть ціну");
+// price = Number(price);
+// console.log(price);
+
+// let element = "70.4556px";
+// // const result = Number.parseInt(element);
+// const result = Number.parseFloat(element);
+// console.log(result);
+
+// let salery = 1300.2558969;
+// salery = salery.toFixed(2);
+// console.log(salery);
+
+// let base = prompt("give a number");
+// base = Number(base);
+// let power = prompt("give a power");
+// power = Number(power);
+// const result = base ** power;
+// console.log(result);
