ba6490f2156f48ff97e380e7ae64ae6afaff4b63
Alona-Liubchenko <aylin888@rambler.ru>
2022-07-12 19:26:31 +0300

    h15

---
+++ script-2.js
@@ -0,0 +1,8 @@
+const friends = ["Mango", "Kiwi", "Poly", "Ajax"];
+console.log(friends.length);
+friends[2] = "java";
+console.table(friends);
+console.log(friends[3]);
+const lastIndex = friends.length - 1;
+
+console.log(lastIndex);
