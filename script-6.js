35bda7d153f37e55a63a528c1f26ec01346cec2e
Alona-Liubchenko <aylin888@rambler.ru>
2022-07-17 00:01:57 +0300

    h19

---
+++ script-6.js
@@ -0,0 +1,23 @@
+// console.log(document);
+
+// const navEl = document.querySelector(".site-nav");
+// console.log(navEl);
+// navEl.classList.add("super-cool"); //добавити
+// navEl.classList.remove("site-nav"); //видалити
+// navEl.classList.replace("super-cool", "qwerty"); //замінити
+// console.log(navEl.classList.contains("qwerty")); //пошук за назвою
+// const firstNavItemEl = navEl.firstElementChild;
+// console.log(firstNavItemEl);
+// console.log(navEl.children);
+
+// const titleEl = document.createElement("h1");
+// titleEl.classList.add("page-title");
+// titleEl.textContent = "Title!)";
+// console.log(titleEl);
+// const imageEl = document.createElement("img");
+// imageEl.src = "./img.jpg";
+// imageEl.alt = "va";
+// imageEl.width = 640;
+// console.log(imageEl.width);
+// console.log("imageEl", imageEl);
+// document.body.appendChild(imageEl);
